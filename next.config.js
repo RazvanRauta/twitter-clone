ecc11a4827e41f2fe8783d83dd5b8a21fe0d9e19
Razvan Rauta <razvan@rrazvan.dev>
2021-11-28 20:36:51 +0300

    feat(sidebar): finished the side bar

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = {
 
   reactStrictMode: true,
   images: {
-    domains: ['rb.gy'],
+    domains: ['rb.gy', 'avatars0.githubusercontent.com'],
   },
 };
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -4,7 +4,9 @@
  *  Time: 18:48
  */
 
+import noop from 'lodash/noop';
 import React, { ReactElement } from 'react';
+import { HiOutlineDotsHorizontal } from 'react-icons/hi';
 
 import styles from './styles.module.css';
 
@@ -37,6 +39,23 @@ export default function SideBar({ sideBarLinks }: ISideBarProps): ReactElement {
               {...link}
             />
           ))}
+      </div>
+      <button className={styles['tweet-button']}>Tweet</button>
+      <div className={styles['profile-details-container']} onClick={() => noop}>
+        <NextImage
+          src={'https://avatars0.githubusercontent.com/u/40596596?v=4'}
+          alt='User Image'
+          useSkeleton
+          className='my-3 xl:mr-2.5'
+          imgClassName='h-10 w-10 rounded-full'
+          width={40}
+          height={40}
+        />
+        <div className='hidden leading-5 xl:inline'>
+          <h4 className='font-bold'>Username</h4>
+          <p className='text-[#6e767d]'>@user-tag</p>
+        </div>
+        <HiOutlineDotsHorizontal className='hidden h-5 ml-10 xl:inline' />
       </div>
     </div>
   );
