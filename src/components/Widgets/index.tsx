80c24f97bead92d65be60874b8a3229454fc4fe2
Razvan Rauta <rrauta@klika-tech.com>
2021-11-30 12:43:41 +0300

    feat(widgets): added widgets component

--- src/components/Widgets/index.tsx
+++ src/components/Widgets/index.tsx
@@ -6,6 +6,10 @@
 
 import type { ReactElement } from 'react';
 import React from 'react';
+import { HiOutlineSearch as SearchIcon } from 'react-icons/hi';
+
+import NextImage from '../NextImage';
+import Trending from '../Trending';
 
 import type { FollowerResults, TrendingResults } from '@/types';
 
@@ -19,17 +23,58 @@ export const Widgets = ({
   trendingResults,
 }: WidgetsProps): ReactElement => {
   return (
-    <div className='hidden'>
-      <ul>
-        {followResults.map((rs) => (
-          <li key={rs.username}>{rs.username}</li>
+    <div className='hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5'>
+      <div className='sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12'>
+        <div className='flex items-center bg-[#202327] p-3 rounded-full relative'>
+          <SearchIcon size='20px' className='z-50 text-gray-500' />
+          <input
+            type='text'
+            className='bg-transparent placeholder-gray-500 outline-none text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent w-full focus:border-[#1d9bf0] rounded-full focus:bg-black focus:shadow-lg'
+            placeholder='Search Twitter'
+          />
+        </div>
+      </div>
+
+      <div className='text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12'>
+        <h4 className='px-4 text-xl font-bold'>{"What's happening"}</h4>
+        {trendingResults.map((result, index) => (
+          <Trending key={index} result={result} />
         ))}
-      </ul>
-      <ul>
-        {trendingResults.map((rs) => (
-          <li key={rs.description}>{rs.description}</li>
+        <button className='hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light'>
+          Show more
+        </button>
+      </div>
+
+      <div className='text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12'>
+        <h4 className='px-4 text-xl font-bold'>Who to follow</h4>
+        {followResults.map((result, index) => (
+          <div
+            className='hover:bg-white hover:bg-opacity-[0.03] px-4 py-2 cursor-pointer transition duration-200 ease-out flex items-center'
+            key={index}
+          >
+            <NextImage
+              src={result.userImg}
+              width={50}
+              height={50}
+              objectFit='cover'
+              className='rounded-full'
+              alt='Profile Pic'
+            />
+            <div className='ml-4 leading-5 group'>
+              <h4 className='font-bold group-hover:underline'>
+                {result.username}
+              </h4>
+              <h5 className='text-gray-500 text-[15px]'>{result.tag}</h5>
+            </div>
+            <button className='ml-auto bg-white text-black rounded-full font-bold text-sm py-1.5 px-3.5'>
+              Follow
+            </button>
+          </div>
         ))}
-      </ul>
+        <button className='hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light'>
+          Show more
+        </button>
+      </div>
     </div>
   );
 };
